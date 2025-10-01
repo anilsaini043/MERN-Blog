d5109860e1000e1433f8db508f59e14088b30851
anilsaini043 <anilsaini043@gmail.com>
2025-10-01 23:18:59 +0530

    Fixed CORS issue and allowed request

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./database/db.js";
 import userRoute from "./routes/user.route.js";
+import cors from "cors";
 
 dotenv.config()
 const app = express();
@@ -9,6 +10,10 @@ const app = express();
 // Default middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin : "http://localhost:5173",
+    credentials : true
+}))
 
 // User route path
 app.use("/api/v1/user", userRoute)
